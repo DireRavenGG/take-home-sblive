4636d50957413631ecd55b92d453eccd0eb7c895
DireRavenGG <mattcarter08@icloud.com>
2022-08-25 00:23:53 -0400

    create func to pass state setter

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,27 @@
-import { useEffect, useState } from "react";
+import { ChangeEvent, useEffect, useState } from "react";
 import GamesContainer from "./components/GamesContainer";
 import { Match } from "../types/ResponseData";
 import { getGameData } from "./api/getGameData";
 import "./App.css";
 import { generateQueryString } from "./utils/generateQueryString";
+import FilterContainer from "./components/FilterContainer";
 
 function App() {
   const [games, setGames] = useState<Match[]>();
   const [query, setQuery] = useState({
     date: "2022-08-24",
     state: "",
-    gender: "",
-    status: "",
-    sport: "",
+    gender_id: "",
+    status_id: "",
+    sport_id: "",
   });
+
+  const changeQuery = (e: ChangeEvent<HTMLSelectElement>, id: string) => {
+    setQuery((prevQuery) => ({
+      ...prevQuery,
+      [id]: e.target.value,
+    }));
+  };
 
   useEffect(() => {
     getGameData(generateQueryString(query)).then((res) => {
@@ -23,6 +31,7 @@ function App() {
 
   return (
     <div className="App">
+      <FilterContainer changeQuery={changeQuery} />
       <GamesContainer games={games} />
     </div>
   );
