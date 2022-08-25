c17c4d9b11dd2247e816b82e2bddcee07b1204e6
DireRavenGG <mattcarter08@icloud.com>
2022-08-25 11:49:52 -0400

    create scoreboard component

---
+++ src/components/Scoreboard.tsx
@@ -0,0 +1,11 @@
+import { Match } from "../../types/ResponseData";
+
+type ScoreboardProps = {
+  match: Match;
+};
+
+const Scoreboard = ({ match }: ScoreboardProps) => {
+  return <div></div>;
+};
+
+export default Scoreboard;
