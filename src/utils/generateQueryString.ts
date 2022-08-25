7e0c9a92ea079ab4f8494cb97fa96fe5a97b213a
DireRavenGG <mattcarter08@icloud.com>
2022-08-24 22:59:35 -0400

    change type for generateQueryString

--- src/utils/generateQueryString.ts
+++ src/utils/generateQueryString.ts
@@ -1,26 +1,11 @@
 import queryString from "query-string";
 
-type generateQueryStringProps = {
-  date: string;
-  state: string;
-  gender: string;
-  status: string;
-  sport: string;
-};
+type generateQueryStringProps = Record<string, string>;
 
-export const generateQueryString = ({
-  date,
-  state,
-  gender,
-  status,
-  sport,
-}: generateQueryStringProps) => {
-  const string = queryString.stringify(
-    { status_id: status, date, state, gender_id: gender, sport_id: sport },
-    {
-      skipEmptyString: true,
-    }
-  );
+export const generateQueryString = (queries: generateQueryStringProps) => {
+  const string = queryString.stringify(queries, {
+    skipEmptyString: true,
+  });
   const url = "/v2/games?" + string;
   return url;
 };
