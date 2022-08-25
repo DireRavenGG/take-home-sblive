db15c0900b49563003ac19c7def9f2a556b8a68c
DireRavenGG <mattcarter08@icloud.com>
2022-08-25 12:43:10 -0400

    add new breakpoints

--- src/index.tsx
+++ src/index.tsx
@@ -2,16 +2,25 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 import { QueryClient, QueryClientProvider } from "react-query";
 export const queryClient = new QueryClient();
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
+
+const breakpoints = {
+  sm: "320px",
+  md: "650px",
+  lg: "960px",
+  xl: "1200px",
+  "2xl": "1536px",
+};
+const theme = extendTheme({ breakpoints });
 root.render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <ChakraProvider>
+      <ChakraProvider theme={theme}>
         <App />
       </ChakraProvider>
     </QueryClientProvider>
