da5c8f112457a4102b3e3fa7fd84ba09fc3b12e4
DireRavenGG <mattcarter08@icloud.com>
2022-08-26 12:01:39 -0400

    remove link box

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,15 +1,25 @@
-import { LinkBox } from "@chakra-ui/react";
+import { useState } from "react";
 import { Match } from "../../types/ResponseData";
+import Scoreboard from "./Scoreboard";
 import TeamsContainer from "./TeamsContainer";
 type GameProps = {
   game: Match;
 };
 
 const Game = ({ game }: GameProps) => {
+  const [openScoreboard, setOpenScoreboard] = useState(false);
+
+  const scoreboardHandler = () => {
+    setOpenScoreboard((prevOpenScoreboard) => !prevOpenScoreboard);
+  };
+
   return (
-    <LinkBox>
-      <TeamsContainer teams={game.game_teams} match={game} />
-    </LinkBox>
+    <div>
+      <button style={{ width: "100%" }} onClick={scoreboardHandler}>
+        <TeamsContainer teams={game.game_teams} match={game} />
+      </button>
+      {openScoreboard ? <Scoreboard match={game} /> : null}
+    </div>
   );
 };
 
